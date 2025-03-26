c2fec18e681f53ce7fbbc070a1712c8680f8f718
joebotseke <brandon.bots777@gmail.com>
2025-03-26 16:55:13 +0100

    Epreuves CA, terre- exo5 - pair impair terminé

--- epreuves/terre/pair_impair/pairImpair.js
+++ epreuves/terre/pair_impair/pairImpair.js
@@ -1,25 +1,24 @@
 const { argv } = require('node:process');
 
-function pairImpair(nombre){
-    let messageErreur = "Tu ne me la mettras pas à l'envers";
-    if(nombre %2== 0){
+function pairImpair(argument){
+    let nombre = parseInt(argument);
+
+    // Vérification si l'argument est un nombre
+    if (isNaN(nombre)) {
+        console.log("Tu ne me la mettras pas à l'envers");
+        return;
+    }
+
+    // Traitement de 0 comme pair
+    if (nombre % 2 == 0) {
         console.log("pair");
-    }
-    else if(nombre == 0 ){
-        console.log(messageErreur)
-    }
-  
-    else if (nombre %3==0){
-        console.log("Impair");
-    }
-    
-    else if (nombre < 0 || nombre == NaN || nombre == undefined){
-        console.log(messageErreur);
-    }
-    else {
-        console.log(messageErreur);
+    } else {
+        console.log("impair");
     }
 }
-
-
-pairImpair(argv[2]);
+// Vérification si un argument a été passé
+if (argv.length < 3) {
+    console.log("Il faut passer un argument");
+} else {
+    pairImpair(argv[2]);
+}
