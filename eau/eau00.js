3724e4af88698451dff414c3cd28d29002496196
Joe Botseke <brandon.bots777@gmail.com>
2025-05-29 14:25:15 +0200

    feat: Display 3-digit combination

---
+++ eau/eau00.js
@@ -0,0 +1,22 @@
+/**
+ * program that displays all the different possible combinations of three digits in ascending order. 
+ */
+
+// Solving
+function ThreeDigitCombination(){
+    let combination = "";
+    for (let i = 0; i <=9; i++){
+        for (let j = i+1; j<=9; j++){
+            for (let k = j+1; k<=9; k++){
+                combination += `${i}${j}${k}`;
+                if (!(i === 7 && j === 8 && k === 9)){
+                    combination += ", ";
+                }
+            }
+        }
+    }
+    return combination;
+}
+
+// Display 
+console.log(ThreeDigitCombination());
