3d1b8bb7944534fb0d0eebe4fad2dcebc4e44c38
Joe Botseke <brandon.bots777@gmail.com>
2025-08-15 01:41:38 +0200

    feat: Bubble sort

---
+++ eau/eau12.js
@@ -0,0 +1,73 @@
+// === Utils ===
+function getLength(arguments) {
+    let argumentsLength = 0;
+    for (const char of arguments) {
+        argumentsLength++;
+    }
+    return argumentsLength;
+}
+
+function sliceFrom(arguments, index) {
+    let newArguments = [];
+    for (let i = index; i < getLength(arguments); i++) {
+        newArguments.push(arguments[i]);
+    }
+    return newArguments;
+}
+
+function isValidArguments(arguments, minLength) {
+    if (getLength(arguments) < minLength) {
+        console.error("error");
+        return false;
+    }
+    return true;
+}
+
+function containsInvalidNumber(arguments) {
+    for (const argument of arguments) {
+        if (isNaN(Number(argument))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function absoluteDifference(a, b) {
+    return a > b ? a - b : b - a;
+}
+
+// === Error handling ===
+const userArguments = sliceFrom(process.argv, 2);
+
+if (!isValidArguments(userArguments, 2) || containsInvalidNumber(userArguments)) {
+    console.error("error");
+    process.exit();
+}
+
+// === Parsing ===
+const numbers = [];
+for (let i = 0; i < getLength(userArguments); i++) {
+    numbers.push(Number(userArguments[i]));
+}
+
+// === Solving ===
+function bubbleSort(array) {
+    const arrayLength = getLength(array);
+    for (let i = 0; i < arrayLength; i++){
+        for (let j = 0; j < arrayLength -1 -i; j++){
+            if (array[j] > array[j +1]){
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+// === Display ===
+function main() {
+    const result = bubbleSort(numbers);
+    console.log(result);
+}
+main();
